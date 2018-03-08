5d586ff6563616a3195ac8471ecf0f0570679fbd
SteakingCoder <1941499609@qq.com>
2018-03-09 00:36:32 +0800

    HelloWorld

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "ScalaLearningDemo"
+
+version := "1.0"
+
+scalaVersion := "2.12.4"
+        
---
+++ src/main/scala/com/HelloWorld.scala
@@ -0,0 +1,10 @@
+package scala.com
+
+/**
+  * Created by HuangDanGeeker on 2018/3/9.
+  */
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    println("hello world")
+  }
+}
